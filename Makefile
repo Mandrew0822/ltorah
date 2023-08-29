55610398771dc5f2bd7636d4d735cedf52cd5c3d
Mandrew0822 <110215582+Mandrew0822@users.noreply.github.com>
2023-08-29 17:42:57 -0400

    initial commit

---
+++ Makefile
@@ -0,0 +1,4 @@
+install:
+	sudo cp -r ltorah_dir /usr/local/
+	sudo cp /usr/local/ltorah_dir/ltorah /usr/local/bin
+	@echo "ltorah has been installed"
---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo cp -r ltorah_dir /usr/local/
+sudo cp /usr/local/ltorah_dir/ltorah /usr/local/bin
+
+echo "ltorah has been installed"
